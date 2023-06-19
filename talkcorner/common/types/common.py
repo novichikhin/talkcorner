eec3069eeb944931ec5070295bd1996a753e89a5
novichikhin <novichixinn@gmail.com>
2023-06-19 23:25:23 +0700

    fixed correct filling fields for updates

--- talkcorner/common/types/common.py
+++ talkcorner/common/types/common.py
@@ -7,6 +7,6 @@ class Update(BaseModel):
 
     @root_validator
     def root_validator_update(cls, values: dict[str, Any]) -> dict[str, Any]:
-        if all(not value for value in values.values()):
+        if all(value is None for value in values.values()):
             raise ValueError("at least one value must be filled in")
         return values
