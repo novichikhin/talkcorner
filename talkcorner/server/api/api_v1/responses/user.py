0786cf5a027d45de5c25df1d9a3adbaa45a3f950
novichikhin <novichixinn@gmail.com>
2023-06-25 21:28:01 +0700

    added annotation for user_auth_responses (fix mypy)

--- talkcorner/server/api/api_v1/responses/user.py
+++ talkcorner/server/api/api_v1/responses/user.py
@@ -1,8 +1,10 @@
+from typing import Any, Union
+
 from starlette.status import HTTP_401_UNAUTHORIZED, HTTP_404_NOT_FOUND
 
 from talkcorner.common.types import errors
 
-user_auth_responses = {
+user_auth_responses: dict[Union[int, str], dict[str, Any]] = {
     HTTP_401_UNAUTHORIZED: {
         "description": "Could not validate credentials error",
         "model": errors.NotValidateCredentials
