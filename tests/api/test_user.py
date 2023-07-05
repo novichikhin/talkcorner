ddc1ff0443f76a9cdf93ee15cdadb6d36b9c4594
novichikhin <novichixinn@gmail.com>
2023-07-05 14:17:36 +0700

    added test endpoint for access_token after login

--- tests/api/test_user.py
+++ tests/api/test_user.py
@@ -1,19 +1,13 @@
 import uuid
 
-import pytest
 from httpx import AsyncClient
-from passlib.context import CryptContext
 
 from talkcorner.common.database.holder import DatabaseHolder
 from tests.fixtures.protocols.auth_token import CreateAuthAccessToken, CreateAuthRefreshToken
 from tests.fixtures.protocols.user import CreateUser
 
 
-async def test_login_user(
-        client: AsyncClient,
-        crypt_context: CryptContext,
-        create_user: CreateUser
-):
+async def test_login_user(client: AsyncClient, create_user: CreateUser):
     user = await create_user(password=(password:="qwerty12345"))
 
     response = await client.post(
@@ -22,6 +16,19 @@ async def test_login_user(
             "username": user.username,
             "password": password
         }
+    )
+
+    assert response.status_code == 200
+
+    json = response.json()
+
+    assert (access_token := json["access_token"])
+    assert json["refresh_token"]
+    assert json["token_type"]
+
+    response = await client.get(
+        "/api/v1/user/",
+        headers={"Authorization": f"Bearer {access_token}"}
     )
 
     assert response.status_code == 200
